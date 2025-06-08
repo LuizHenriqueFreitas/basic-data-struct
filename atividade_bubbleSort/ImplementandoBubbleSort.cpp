63c4744989b4f960f0b156c16189a0667f1ba792
Luiz Henrique Freitas <166095630+LuizHenriqueFreitas@users.noreply.github.com>
2025-06-07 22:37:38 -0300

    Create ImplementandoBubbleSort.cpp

---
+++ atividade_bubbleSort/ImplementandoBubbleSort.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+// Implementação da função Bubble Sort
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Troca os elementos
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void mostraArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int arr[] = {7, 3, 9, 1, 5, 2, 8};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Array original: ";
+    mostraArray(arr, n);
+
+    bubbleSort(arr, n);
+
+    cout << "Array ordenado: ";
+    mostraArray(arr, n);
+
+    return 0;
+}
